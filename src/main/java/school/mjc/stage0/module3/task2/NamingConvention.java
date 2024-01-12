56bcc473bb2a6cce4cac732d59cacd5d58f6f23b
Hanna_Urban <huan92@yandex.ru>
2024-01-12 14:55:17 +0300

    Names are corrected.

--- src/main/java/school/mjc/stage0/module3/task2/NamingConvention.java
+++ src/main/java/school/mjc/stage0/module3/task2/NamingConvention.java
@@ -3,14 +3,19 @@ package school.mjc.stage0.module3.task2;
 public class NamingConvention {
     public static void main(String[] args) {
                 int variable = 10;
-                int variable_one = 1;
+                int variableOne = 1;
                 int variableTwo = 2;
-                int variable_three = 3;
+                int variableThree = 3;
                 final int VARIABLE_FOUR = 4;
                 final int VARIABLE_FIVE = 5;
                 int variableSix = 6;
                 int variableSeven = 7;
                 int variableEight = 8;
-        System.out.println(variable+variable_one+variableTwo+variable_three+VARIABLE_FOUR+VARIABLE_FIVE+variableSix+variableSeven+variableEight);
+        System.out.println(variable+variableOne+variableTwo+variableThree+VARIABLE_FOUR+VARIABLE_FIVE+variableSix+variableSeven+variableEight);
     }
 }
+
+
+
+
+
